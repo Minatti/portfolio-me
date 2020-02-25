90a7156872e882a7df529bdce3f88891f9afb353
luancarminatti <luancarminatt@gmail.com>
2020-02-25 19:19:19 -0300

    login auth for admin and home for anonymous done

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    /*Usando o Eloquent
+    
+         DB::table('users')->insert([
+            'name' => 'Luanzera',
+            'email' => 'luancarminatt@gmail.com',
+            'password' => bcrypt('adm123'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Whoami',
+            'email' => 'whoami@gmail.com',
+            'password' => bcrypt('adm1234'),
+        ]);           
+	 */
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin'], function(){
+	$this->get('admin', 'AdminController@index')->name('admin.home');
+});
+*/
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('admin', 'Admin\AdminController@index')->name('admin.home');
+});
+
+
+
+Route::get('/', 'Site\SiteController@index')->name('home');
+
+Auth::routes();
+
+
+//Route::get('/', 'Site\SiteController@index');
+
+//Route::get('/home', function() {
+  //  return view('home');
+//})->name('home')->middleware('auth');
